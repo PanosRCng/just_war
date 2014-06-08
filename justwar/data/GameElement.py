8483b622eaae92ceca5888e82f7ea77e6063bfc3
PanosRCng <panosracing@hotmail.com>
2014-06-08 16:48:17 -0400

    organize to classes, code for easy porting to android with the Pygame Subset for Android

---
+++ justwar/data/GameElement.py
@@ -0,0 +1,23 @@
+import os
+import pygame
+
+class GameElement(pygame.sprite.Sprite):
+
+    def __init__(self):
+
+        #constructor
+
+        pygame.sprite.Sprite.__init__(self)  # call Sprite initializer
+        
+    def load_image(self, name):
+
+	fullname = os.path.join('./data', name)
+
+        try:
+            shape = pygame.image.load(fullname)
+        except pygame.error, message:
+            print 'Cannot load image:', fullname
+            raise SystemExit, message
+
+        return shape
+        
